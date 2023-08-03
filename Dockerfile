f4c7a47ede578b3494cd0b58f370c2eb718c6b53
Blizz <blizzsoftword@gmail.com>
2023-08-02 22:25:56 -0400

    Update the commands and created docker document

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3
+
+WORKDIR /app
+
+COPY . /app/
+
+SHELL [ "/bin/bash", "-c" ]
+
+ENV TOKEN "6592524009:AAFp6S6PwNUXePWHRK_luYCZJObWrHZfPWc"
+
+RUN python3 -m venv /app
+RUN source /app/bin/activate
+# RUN pip install --upgrade pip
+RUN pip install -r /app/requirements.txt
+
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from os import getenv
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_chat_action(update.effective_chat.id, ChatAction.TYPING)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    commands = ""
+    for i in COMMANDS:
+        commands += f"/{i}\n"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"I'm a bot, please talk to me!\nThere are my commands: \n{commands}")
 
 async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_chat_action(update.effective_chat.id, ChatAction.TYPING)
@@ -33,10 +36,37 @@ async def im_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello {user_in_chat.user.first_name}, you aren\'t welcomed!\nPlease enter to {group_id}')
 
+async def get_loc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_chat_action(update.effective_chat.id, ChatAction.FIND_LOCATION)
+    # 23.046640611793258, -81.57829721120095
+    await context.bot.send_location(chat_id=update.effective_chat.id, latitude=23.046640611793258, longitude= -81.57829721120095)
+
+async def get_ctc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_chat_action(update.effective_chat.id, ChatAction.UPLOAD_DOCUMENT)
+    await context.bot.send_contact(chat_id=update.effective_chat.id, phone_number='+53 53013028', first_name='Blizz', last_name='Softword')
+
+async def get_doc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_chat_action(update.effective_chat.id, ChatAction.UPLOAD_DOCUMENT)
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=open('test.txt', 'rb'), caption='Test Document')
+
+COMMANDS = [
+    'start',
+    'im_user',
+    'get_gid',
+    'get_loc',
+    'get_ctc',
+    'get_doc',
+    'get_id'
+]
+
 HANDLERS = [
     CommandHandler('start', start),
     CommandHandler('im_user', im_user),
     CommandHandler('get_gid', get_gid),
+    CommandHandler('get_loc', get_loc),
+    CommandHandler('get_ctc', get_ctc),
+    CommandHandler('get_doc', get_doc),
+
     CommandHandler('get_id', get_id)
 ]
 
@@ -44,9 +74,6 @@ HANDLERS = [
 def main():
     app = ApplicationBuilder().token(getenv('TOKEN')).build()
     app.add_handlers(HANDLERS)
-    # for i in HANDLERS:
-    #     print(f'{i.commands} added')
-    #     app.add_handler(i)
 
     print('Bot Running!')
     app.run_polling()
